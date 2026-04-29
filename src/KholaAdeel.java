bcfff21cefd505f605b46a30d06840e115b96573
khola8001-spec <khola8001@gmail.com>
2026-04-29 14:49:11 -0700

    add class

---
+++ src/KholaAdeel.java
@@ -0,0 +1,15 @@
+public class KholaAdeel {
+    public static void main(String[] args) {
+           String name = "Khola Adeel";
+           String[] moods = {"happy", "sleepy", "chaotic", "productive", "hungry"};
+
+           int i = (int)(Math.random() * moods.length);
+
+           System.out.println(name + " is feeling " + moods[i] + " today");
+           IDK();
+    }
+    public static void IDK(){
+            System.out.print("huh!");
+    }
+
+}
