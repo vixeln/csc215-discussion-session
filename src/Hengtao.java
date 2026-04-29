7a7e425160275848aebb975865fa0364ac6b0b32
taota <taotao1972961019@icloud.com>
2026-04-29 14:37:54 -0700

    The studnet is private

---
+++ src/Hengtao.java
@@ -0,0 +1,16 @@
+public class Hengtao {
+    public String job;
+    private String student;
+
+    public Hengtao (String job, String student){
+        this.job = job;
+        this.student = student;
+    }
+    public String getJob() {
+        return job;
+    }
+
+    public String getStudent() {
+        return student;
+    }
+}
