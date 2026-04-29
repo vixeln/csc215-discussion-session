08fe299374f5d714164abe308d7555142a3e0569
hexadecalice <alicemirga95@gmail.com>
2026-04-29 14:29:36 -0700

    added an interesting comment!

--- src/MyFile.java
+++ src/MyFile.java
@@ -1,6 +1,15 @@
 public class MyFile.java {
 
-int randomNumber;
+int number;
+//he said to write something interesting for my class
+//did you know, that in the byford-dolphin incident
+//a submersible experienced a critical failure, resulting
+//in a man being pulled through a 1x2 hole, and his blood flash boiled
+//it was said his internal organs were launched at speeds of nearly
+//300 mph!!!
+
+
+//hope thats interesting!
 
 
 
