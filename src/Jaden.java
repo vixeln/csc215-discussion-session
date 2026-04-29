a2552f2bfddabae01e1f1393a39603d90e27d87a
qzjo <papasitoocampo07@gmail.com>
2026-04-29 14:48:10 -0700

    Revert "Added another option to the soccer version."
    This reverts commit 78a2ed4fcee5e277e8e5aaee767142ad7dd38e56.

--- src/Jaden.java
+++ src/Jaden.java
@@ -21,7 +21,7 @@ public class Jaden {
 
         if(name.contains("ronaldo") || name.contains("pele") ){
             System.out.println("Correct!");
-        }else if (name.contains("messi") || name.contains("yamal"))
+        }else if (name.contains("messi"))
         {
             System.out.println("hell no");
         }else {
