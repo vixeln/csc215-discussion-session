96d028e7ad28b8d6cf9fa7d4f16c2334b49e5aea
jabir <jabirbinumair493@gmail.com>
2026-04-29 14:54:30 -0700

    another println commit.

--- src/Jabir.java
+++ src/Jabir.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 public class ClassExample {
     public static void main(String[] args) {
         System.out.println("Hello CSC 215");
-        System.out.println("knabjgc")
+        System.out.println("knabjgc");
+        System.out.println("Hello world");
+
     }
 }
