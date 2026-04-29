5e768852eca418c098cab0425e296384e6f93293
Adam <adam.de-lacheisserie-levy@epitech.eu>
2026-04-29 14:35:29 -0700

    feat: add Adam's class to the project

---
+++ src/AdamClass.java
@@ -0,0 +1,17 @@
+public class AdamClass {
+    private String nationality;
+    private String lastName;
+
+    public AdamClass() {
+        this.nationality = "French";
+        this.lastName = "De Lacheisserie";
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
